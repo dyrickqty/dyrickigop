d34ef28b9673fe0093a284719b0cab6230d0c990
Dyrick Lobusta <116326903+dyrickqty@users.noreply.github.com>
2022-10-21 17:23:48 +0800

    Add files via upload

---
+++ OrganizationProfile.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmRegistration : Form
+    {
+        private string _FullName;
+        private int _Age;
+        private long _ContactNo;
+        private long _StudentNo;
+
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+        public long StudentNumber(string studNum)
+        {
+            if (Regex.IsMatch(studNum, @"^[0-9]{1,12}$"))
+            {
+                _StudentNo = long.Parse(studNum);
+            }
+            else
+            {
+                throw new FormatException("Only Numbers are allowed in Student Number Box");
+            }
+            
+
+            return _StudentNo;
+        }
+
+        public long ContactNo(string Contact)
+        {
+            if (Regex.IsMatch(Contact, @"^[0-9]{1,11}$"))
+            {
+                _ContactNo = long.Parse(Contact);
+            }
+            else
+            {
+                throw new FormatException("Incorrect Input in Contact Number");
+            }    
+
+            return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") && Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") && Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+            {
+                _FullName = LastName + ", " + FirstName + " " + MiddleInitial;
+            }
+            else
+            {
+                throw new FormatException("Only Letters are allowed in the Name box");
+            }
+
+            return _FullName;
+        }
+
+        public int Age(string age)
+        {
+            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+            {
+                _Age = Int32.Parse(age);
+            }
+            else
+            {
+                throw new FormatException("Only Numbers are allowed in Age Box");
+            }
+
+            return _Age;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProgram = new string[]{
+                "BS Information Technology",
+                "BS Computer Science",
+                "BS Information Systems",
+                "BS in Accountancy",
+                "BS in Hospitality Management",
+                "BS in Tourism Management"
+            };      
+                for (int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+        }
+
+        private void txtContactNo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                StudentInformationClass.SetFullName = FullName(txtLastName.Text, txtFirstName.Text, txtMiddleInitial.Text);
+                StudentInformationClass.SetStudentNo = StudentNumber(txtStudentNo.Text);
+                StudentInformationClass.SetProgram = cbPrograms.Text;
+                StudentInformationClass.SetGender = cbGender.Text;
+                StudentInformationClass.SetContactNo = ContactNo(txtContactNo.Text);
+                StudentInformationClass.SetAge = Age(txtAge.Text);
+                StudentInformationClass.SetBirthDay = datePickerBirthday.Value.ToString("yyyy-MM-dd");
+                frmConfirmation frm = new frmConfirmation();
+                frm.ShowDialog();
+                txtLastName.Clear();
+                txtFirstName.Clear();
+                txtMiddleInitial.Clear();
+                txtStudentNo.Clear();
+                txtContactNo.Clear();
+                txtAge.Clear();
+                datePickerBirthday.Value = DateTime.Now;
+                cbGender.SelectedIndex = -1;
+                cbPrograms.SelectedIndex = -1;
+
+
+
+            }
+        catch (FormatException dyr)
+        {
+                MessageBox.Show(dyr.Message);
+                MessageBox.Show("Ekis Ka par");
+        }
+
+        }
+
+        private void cbGender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtStudentNo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
