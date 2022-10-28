72db339138e5830cd7953482c5b83c90b3bfe472
Dyrick Lobusta <116326903+dyrickqty@users.noreply.github.com>
2022-10-28 13:16:02 +0800

    Add files via upload

---
+++ MyThreadClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace FrmBasicThreadLobusta
+{
+    class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int loopingCount = 0; loopingCount<6; loopingCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopingCount);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
